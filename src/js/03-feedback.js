50dbf7722e58d5ca4461fc94d61cb0b6e9982d76
Maksym Hanzha <101345604+MaksymHanzha@users.noreply.github.com>
2022-09-08 22:47:52 +0300

    --SAVE_3--

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const formElements = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+formElements.addEventListener('submit', onFormSubmit);
+formElements.addEventListener('input', throttle(onInputChange, 500));
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) fillFormCurrentData();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onInputChange(e) {
+  const formData = new FormData(formElements);
+  const formDataJSON = JSON.stringify(Object.fromEntries(formData));
+  localStorage.setItem(STORAGE_KEY, formDataJSON);
+}
+
+function fillFormCurrentData() {
+  try {
+    const savedDataObj = JSON.parse(savedData);
+
+    Object.keys(savedDataObj).forEach(
+      key => (formElements[key].value = savedDataObj[key])
+    );
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
