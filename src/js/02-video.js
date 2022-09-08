efe7023224cb46f1bef3c1bcc1dc853ceeaeb004
Maksym Hanzha <101345604+MaksymHanzha@users.noreply.github.com>
2022-09-08 22:20:02 +0300

    --SAVE_2--

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+if (localStorage.getItem(CURRENT_TIME)) {
+  setValidTime(localStorage.getItem(CURRENT_TIME));
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(CURRENT_TIME, seconds);
+}
+
+function setValidTime(seconds) {
+  player
+    .setCurrentTime(seconds)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    });
+}
